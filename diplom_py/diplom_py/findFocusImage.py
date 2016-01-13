d043a287fced2a69a6d4f933f64e64c3e68ee671
Kirill <ksfonmu@gmail.com>
2016-01-13 15:22:01 +0200

    13.01.2016 15:21 atWork

--- diplom_py/diplom_py/findFocusImage.py
+++ diplom_py/diplom_py/findFocusImage.py
@@ -1,12 +1,47 @@
 ﻿import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from matplotlib.backend_bases import NavigationToolbar2
+from matplotlib.widgets import Button
 import sys
 
-
 images = {}
+imageNames = []
+counter = 0
+home = NavigationToolbar2.home
+forward = NavigationToolbar2.forward
+back = NavigationToolbar2.back
 
-def findFocus(imagePathName):
+resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+button = Button(resetax, 'Reset', color='lightgoldenrodyellow', hovercolor='0.975')
+
+
+def newBack(self, *args, **kwargs):
+    global counter
+    if counter != 0:
+        counter -= 1
+        print(counter)
+        findFocus(imageNames[counter],True)    
+        back(self, *args, **kwargs)
+        
+def newForward(self, *args, **kwargs):
+    global counter
+    if counter != len(imageNames)-1:
+        counter += 1
+        print(counter)
+        findFocus(imageNames[counter],True)   
+        forward(self, *args, **kwargs)
+
+def newHome(self, *args, **kwargs):
+    findFocus(imageNames[0],True)
+    home(self, *args, **kwargs)
+
+NavigationToolbar2.home = newHome
+NavigationToolbar2.forward = newForward
+NavigationToolbar2.back = newBack
+
+
+def findFocus(imagePathName,showPlot):
     img = cv2.imread(imagePathName,0)
     grd_mat = img.copy()
     laplacian = cv2.Laplacian(img,cv2.CV_64F)
@@ -17,19 +52,25 @@ def findFocus(imagePathName):
     images[gradientAmplValue] = imagePathName
     print(imagePathName)
     print(gradientAmplValue)
+    if(showPlot == True):
+        plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+        plt.title('Original'), plt.xticks([]), plt.yticks([])   
+        plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+        plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+        plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+        plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+        plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+        plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
 
-    plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
-    plt.title('Original'), plt.xticks([]), plt.yticks([])
-    plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
-    plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
-    plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
-    plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
-    plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
-    plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
-   
-    plt.show()
+        return plt.show()
+    else:
+        return gradientAmplValue
+    
 
-    return gradientAmplValue
+    
+
+
+
 
 def showBestFocusImage(bestGradientValue):
     print('\nbest focus image ')
@@ -38,4 +79,4 @@ def showBestFocusImage(bestGradientValue):
     cv2.imshow('Best Focus Image',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
+button.on_clicked(showBestFocusImage)
--- diplom_py/diplom_py/main.py
+++ diplom_py/diplom_py/main.py
@@ -3,20 +3,22 @@ import numpy as np
 import cv2
 import glob
 import sys
+from matplotlib import pyplot as plt
+
 images = findFocusImage.images
+imageNames = findFocusImage.imageNames
+
 
 def loadImages(pathDir):
     for file in glob.glob(pathDir):
-        min = findFocusImage.findFocus(file)
-        
-    for key in images.keys():
-        if key <= min:
-            min = key
-    findFocusImage.showBestFocusImage(min)
+       findFocusImage.findFocus(file,False)
+       imageNames.append(file)
 
 if __name__ == "__main__":  
     if len(sys.argv) > 1:  
         path = sys.argv[1]+'*.jpg'
         loadImages(path)
+        findFocusImage.findFocus(imageNames[0],True)
     else:
-       loadImages('./*.jpg')
+        loadImages('./*.jpg')
+        findFocusImage.findFocus(imageNames[0],True)
